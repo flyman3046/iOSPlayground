36c0f08749de9d43d2b6750962a4d6d535c1ae1d
Fei Zheng <fei@Feis-MacBook-Pro.local>
2016-12-03 19:43:17 -0800

    Landscape and protrait practice

--- iOSPlayground/PodViewController.swift
+++ iOSPlayground/PodViewController.swift
@@ -12,14 +12,14 @@ import Alamofire
 import AlamofireImage
 
 class PodViewController: UIViewController {
-
+    
     @IBOutlet weak var myRequestButton: UIButton!
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         print("Navigate to PodViewController")
         setupInfoView()
@@ -43,7 +43,7 @@ class PodViewController: UIViewController {
         
         self.view.addSubview(infoView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -73,15 +73,15 @@ class PodViewController: UIViewController {
             }
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
